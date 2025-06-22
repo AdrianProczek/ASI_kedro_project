c99c735bc918a639314b01b192385229310e4aa8
adrian.proczek <adrian.proczek@gmail.com>
2025-06-22 16:40:49 +0200

    Initial commit

--- predict-job-change/src/predict_job_change/pipelines/data_preparation/nodes.py
+++ predict-job-change/src/predict_job_change/pipelines/data_preparation/nodes.py
@@ -1,35 +1,78 @@
+# """
+# This is a boilerplate pipeline 'data_preparation'
+# generated using Kedro 0.19.13
+# """
+#
+# import pandas as pd
+#
+# def merge_datasets(df_initial: pd. DataFrame, df_new: pd. DataFrame) -> pd. DataFrame:
+#     return pd.concat ( [df_initial, df_new])
+#
+# def clean_data(df: pd. DataFrame) -> pd. DataFrame:
+#     # Remove duplicates
+#     return df.drop_duplicates (subset= ["Date"])
+#
+# def generate_datetime_features(df: pd.DataFrame) -> pd.DataFrame:
+#
+#     ### Extract date time features
+#     df ['Date'] = pd.to_datetime(df[' Date'])
+#
+#     # Add a column for year
+#     df['year_num'] = df['Date'].dt.year
+#
+#     # Add a column for month
+#     df['month _num'] = df['Date'].dt.month
+#
+#     # Add a column for day of week
+#     df['dayofweek num'] = df['Date'].dt.dayofweek
+#
+#     # Add a column for day of month
+#     df['dayofmonth'] = df['Date'].dt.day
+#
+#     # Add a column for day of year
+#     df['dayofyear_num'] = df['Date'].dt.day_of_year
+#
+#     return df
+
+
+
 """
 This is a boilerplate pipeline 'data_preparation'
 generated using Kedro 0.19.13
 """
 
 import pandas as pd
+# from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score
 
-def merge_datasets(df_initial: pd. DataFrame, df_new: pd. DataFrame) -> pd. DataFrame:
-    return pd.concat ( [df_initial, df_new])
+def clean_missing_values(data: pd.DataFrame) -> pd.DataFrame:
+    data["gender"] = data["gender"].fillna("No data")
+    data["company_size"] = data["company_size"].fillna("No data")
+    return data
 
-def clean_data(df: pd. DataFrame) -> pd. DataFrame:
-    # Remove duplicates
-    return df.drop_duplicates (subset= ["Date"])
+def encode_and_scale(data: pd.DataFrame) -> pd.DataFrame:
+    data = pd.get_dummies(data)
+    scaler = MinMaxScaler()
+    data[data.columns] = scaler.fit_transform(data)
+    return data
 
-def generate_datetime_features(df: pd.DataFrame) -> pd.DataFrame:
+# def split_features_target(data: pd.DataFrame) -> tuple:
+#     X = data.drop("target", axis=1)
+#     y = data["target"]
+#     return X, y
 
-    ### Extract date time features
-    df ['Date'] = pd.to_datetime(df[' Date'])
-
-    # Add a column for year
-    df['year_num'] = df['Date'].dt.year
-
-    # Add a column for month
-    df['month _num'] = df['Date'].dt.month
-
-    # Add a column for day of week
-    df['dayofweek num'] = df['Date'].dt.dayofweek
-
-    # Add a column for day of month
-    df['dayofmonth'] = df['Date'].dt.day
-
-    # Add a column for day of year
-    df['dayofyear_num'] = df['Date'].dt.day_of_year
-
-    return df
+# def split_train_test(X: pd.DataFrame, y: pd.Series) -> tuple:
+#     return train_test_split(X, y, test_size=0.2, random_state=42)
+#
+# def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> RandomForestClassifier:
+#     model = RandomForestClassifier(random_state=42)
+#     model.fit(X_train, y_train)
+#     return model
+#
+# def evaluate_model(model: RandomForestClassifier, X_test: pd.DataFrame, y_test: pd.Series) -> float:
+#     y_pred = model.predict(X_test)
+#     acc = accuracy_score(y_test, y_pred)
+#     print(f"Accuracy: {acc:.2f}")
+#     return acc
--- predict-job-change/src/predict_job_change/pipelines/data_preparation/pipeline.py
+++ predict-job-change/src/predict_job_change/pipelines/data_preparation/pipeline.py
@@ -1,30 +1,88 @@
+# """
+# This is a boilerplate pipeline 'data_preparation'
+# generated using Kedro 0.19.13
+# """
+#
+# from kedro.pipeline import node, Pipeline, pipeline  # noqa
+# from .nodes import merge_datasets, clean_data, generate_datetime_features
+#
+#
+# def create_pipeline(**kwargs) -> Pipeline:
+#     return pipeline([
+#         node(
+#             func=merge_datasets,
+#             inputs=["data1", "data2"],
+#             outputs="merged_data",
+#             name="merge_datasets_node"
+#         ),
+#         node(
+#             func=clean_data,
+#             inputs="merged_data",
+#             outputs="cleaned_data",
+#             name="cleaned_datasets_node"
+#         ),
+#         node(
+#             func=generate_datetime_features,
+#             inputs="cleaned_data",
+#             outputs="features_data",
+#             name="generate_features_node"
+#         )
+#     ])
+
+
+
 """
 This is a boilerplate pipeline 'data_preparation'
 generated using Kedro 0.19.13
 """
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
-from .nodes import merge_datasets, clean_data, generate_datetime_features
-
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import (
+    clean_missing_values,
+    encode_and_scale
+    # split_features_target,
+    # split_train_test,
+    # train_model,
+    # evaluate_model
+)
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
-            func=merge_datasets,
-            inputs=["data1", "data2"],
-            outputs="merged_data",
-            name="merge_datasets_node"
+            func=clean_missing_values,
+            inputs="main_dataset",
+            outputs="clean_data",
+            name="clean_missing_values_node"
         ),
         node(
-            func=clean_data,
-            inputs="merged_data",
-            outputs="cleaned_data",
-            name="cleaned_datasets_node"
-        ),
-        node(
-            func=generate_datetime_features,
-            inputs="cleaned_data",
-            outputs="features_data",
-            name="generate_features_node"
+            func=encode_and_scale,
+            inputs="clean_data",
+            outputs="encoded_scaled_data",
+            name="encode_and_scale_node"
         )
+        # node(
+        #     func=split_features_target,
+        #     inputs="encoded_scaled_data",
+        #     outputs=["X", "y"],
+        #     name="split_features_target_node"
+        # )
+        # ),
+        # node(
+        #     func=split_train_test,
+        #     inputs=["X", "y"],
+        #     outputs=["X_train", "X_test", "y_train", "y_test"],
+        #     name="split_train_test_node"
+        # ),
+        # node(
+        #     func=train_model,
+        #     inputs=["X_train", "y_train"],
+        #     outputs="trained_model",
+        #     name="train_model_node"
+        # ),
+        # node(
+        #     func=evaluate_model,
+        #     inputs=["trained_model", "X_test", "y_test"],
+        #     outputs="model_accuracy",
+        #     name="evaluate_model_node"
+        # )
     ])
