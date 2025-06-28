be2fcbf9b9bad535307a42c4d4080f41d73f1900
adrian.proczek <adrian.proczek@gmail.com>
2025-06-28 13:01:07 +0200

    Change language

--- predict-job-change/streamlit_app.py
+++ predict-job-change/streamlit_app.py
@@ -9,22 +9,22 @@ from src.predict_job_change.pipelines.data_preparation.nodes import encode_categ
 model_path = "data/06_models/dt_model.pkl"
 model = pickle.load(open(model_path, "rb"))
 
-st.title("Czy zmienisz prace")
+st.title("Do you change job?")
 
 
 city_development_index = st.slider("City Development Index", 0.0, 1.0, 0.5, step=0.01)
-gender = st.selectbox("Płeć", ["Male", "Female", "Other"])
-relevent_experience = st.selectbox("Czy masz doświadczenie w branży?",
+gender = st.selectbox("Gender", ["Male", "Female", "Other"])
+relevent_experience = st.selectbox("Relevent experience?",
                                    ["Has relevent experience", "No relevent experience"])
-enrolled_university = st.selectbox("Status uczelni", ["no_enrollment", "Full time course", "Part time course"])
-education_level = st.selectbox("Poziom wykształcenia", ["Primary School", "High School", "Graduate", "Masters", "Phd"])
-major_discipline = st.selectbox("Kierunek studiów",
+enrolled_university = st.selectbox("University status", ["no_enrollment", "Full time course", "Part time course"])
+education_level = st.selectbox("Education level", ["Primary School", "High School", "Graduate", "Masters", "Phd"])
+major_discipline = st.selectbox("Field of study",
                                 ["STEM", "Business Degree", "Arts", "Humanities", "Other", "No Major"])
-experience = st.selectbox("Liczba lat doświadczenia", [f"{i}" for i in range(1, 21)] + ["<1", ">20"])
-company_size = st.selectbox("Rozmiar firmy",
+experience = st.selectbox("Work experience in years", [f"{i}" for i in range(1, 21)] + ["<1", ">20"])
+company_size = st.selectbox("Company size",
                             ["<10", "10-49", "50-99", "100-500", "500-999", "1000-4999", "5000-9999", "10000+"])
-company_type = st.selectbox("Typ firmy", ["Private", "Public", "Startup", "NGO", "Other"])
-last_new_job = st.selectbox("Czas od ostatniej zmiany pracy", ["Migf", "1", "2", "3", "4", ">4"])
+company_type = st.selectbox("Company type", ["Private", "Public", "Startup", "NGO", "Other"])
+last_new_job = st.selectbox("Time since last job change", ["Migf", "1", "2", "3", "4", ">4"])
 
 input_data = pd.DataFrame({
     "city_development_index": [city_development_index],
@@ -39,15 +39,15 @@ input_data = pd.DataFrame({
     "last_new_job": [last_new_job],
 })
 
-st.write("### Twoje dane:")
+st.write("### Your Data:")
 st.dataframe(input_data)
 
-if st.button("Przewiduj"):
+if st.button("Predict"):
 
     x = encode_categorical_columns(input_data)
     y_pred = model.predict(x)
 
     if int(np.array(y_pred).item()) == 1:
-        st.success("Model przewiduje: Zmienisz pracę")
+        st.success("Model prediction: You will change job")
     else:
-        st.error("Model przewiduje: Nie zmienisz pracy")
+        st.error("Model prediction: You will not change job")
